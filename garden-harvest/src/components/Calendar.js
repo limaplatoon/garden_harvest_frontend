32fae868545ace368cddd5b335aa53fcfe2ce518
D-Lindberg <58619523+D-Lindberg@users.noreply.github.com>
2020-08-10 04:07:27 -0500

    added a new status category of to_be_scheduled, and corrected a Hex color error.

--- garden-harvest/src/components/Calendar.js
+++ garden-harvest/src/components/Calendar.js
@@ -27,43 +27,52 @@ const mapApiData = (data) => {
     let plant = slotdata.plant_zone.plant
     let category = {text: slot.name, id: slot.id, color: slot.color}
     let event ={Id: id, Subject: plant.common_name, IsAllDay: true, TaskId: slot.id, Location: slot.location_description, Description: `${plant.sowing} ${plant.spacing}`}
-    if (slotdata.requires_seeding === true && (slotdata.date_seeded == null || slotdata.date_seeded === "") ) {
+    if (slotdata.created_at == null || slotdata.created_at === ''){
       category.groupId = 1
+      harvesting.push(category)
+      event.StartTime = new Date.now()
+      let endDate = new Date.now()
+      event.EndTime = new Date(endDate.setDate(endDate.getDate() + 7))
+      event.ProjectId = 1
+      events.push(event)
+    } else if (slotdata.requires_seeding === true && (slotdata.date_seeded == null || slotdata.date_seeded === "") ) {
+      category.groupId = 2
       seeding.push(category)
       event.StartTime = new Date(slotdata.created_at)
       let endDate = new Date(slotdata.created_at)
       event.EndTime = new Date(endDate.setDate(endDate.getDate() + 14))
-      event.ProjectId = 1
+      event.ProjectId = 2
       events.push(event)
     } else if (slotdata.requires_seeding === true && (slotdata.date_planted == null || slotdata.date_planted === "")){
-      category.groupId = 2
+      category.groupId = 3
       transplanting.push(category)
       let startDate = new Date(slotdata.created_at)
       event.StartTime = new Date(startDate.setDate(startDate.getDate() + 28))
       let endDate = new Date(slotdata.date_seeded)
       event.EndTime = new Date(endDate.setDate(endDate.getDate()+42))
-      event.ProjectId = 2
+      event.ProjectId = 3
       events.push(event)
     } else if (slotdata.requires_seeding === true ){
-      category.groupId = 4
+      category.groupId = 5
       harvesting.push(category)
       event.StartTime = new Date(slotdata.harvest_date_min)
       event.EndTime = new Date(slotdata.harvest_date_max)
-      event.ProjectId = 4
+      event.ProjectId = 5
       events.push(event)
     } else if (slotdata.requires_seeding === false && (slotdata.date_planted == null || slotdata.date_planted === "")){
-      category.groupId = 3
+      category.groupId = 4
       planting.push(category)
       event.StartTime = new Date(slotdata.created_at)
       let endDate = new Date(slotdata.created_at)
       event.EndTime = new Date(endDate.setDate(endDate.getDate() + 14))
-      event.ProjectId = 3
+      event.ProjectId = 4
       events.push(event)
     } else {
-      category.groupId = 4
+      category.groupId = 5
       harvesting.push(category)
       event.StartTime = new Date(slotdata.harvest_date_min)
       event.EndTime = new Date(slotdata.harvest_date_max)
+      event.ProjectId = 5
       events.push(event)
     }
   }
@@ -80,10 +89,11 @@ class Calendar extends Component {
   constructor(props) {
     super(props)
     let projectData = [
-      { text: "To be Seeded", id: 1, color: "#EA9A8A" },
-      { text: "To be Transplanted", id: 2, color: "#C84A31" },
-      { text: "To be Planted", id: 3, color: "#C08BD7C" },
-      { text: "To be Harvested", id: 4, color: "#CB563E" }
+      { text: "To be Scheduled", id: 1, color: "#48502E" },
+      { text: "To be Seeded", id: 2, color: "#EA9A8A" },
+      { text: "To be Transplanted", id: 3, color: "#C84A31" },
+      { text: "To be Planted", id: 4, color: "#C0BD7C" },
+      { text: "To be Harvested", id: 5, color: "#CB563E" }
     ]
     this.state = { events: [], categories: [], projectData: projectData}
   }
@@ -113,7 +123,7 @@ async componentDidMount() {
               field='ProjectId'
               title='Activity'
               name='Projects'
-              allowMultiple={false}
+              allowMultiple={true}
               dataSource={this.state.projectData}
               textField='text'
               idField='id'
