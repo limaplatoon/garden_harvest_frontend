ad3840e123c37a5c30fdc20229e8648cfdbce4f2
Chris Howell <crhowell@users.noreply.github.com>
2020-08-10 11:29:41 -0700

    Update all fetch calls with axios and add key to PlantCard

--- garden-harvest/src/api/calendarAPI.js
+++ garden-harvest/src/api/calendarAPI.js
@@ -1,14 +1,5 @@
-//this is where Api calls to the backend specificly for the calendar.
-//it will eventually be merged with the other api calls.
-
-const BASE_URL = 'http://127.0.0.1:8000/api/'
+import axiosInstance from './axiosAPI';
 
 export const fetchAllEvents = async () => {
-  let response = await fetch(`${BASE_URL}calendarEvents/`, {
-    headers: {'Content-Type': 'application/json'},
-    method: 'GET',
-  })
-  let data = await response.json()
-  console.log(data)
-  return data
+  return await axiosInstance.get('calendarEvents/').then(res => res.data)
 }
