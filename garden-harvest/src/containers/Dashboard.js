c01eb066086ebe72c579ff111b93e201bf7d9e46
Julian West <west221b@gmail.com>
2020-08-10 16:06:29 -0400

    Make logout button work

--- garden-harvest/src/containers/Dashboard.js
+++ garden-harvest/src/containers/Dashboard.js
@@ -5,50 +5,48 @@ import settings from '../image_SVG_files/settings.svg'
 import logout from '../image_SVG_files/x-circle.svg'
 import info from '../image_SVG_files/info.svg'
 import "./Dashboard.css";
-import { LinkContainer } from "react-router-bootstrap";
-import { Form, FormControl, Button, Row, Col, ListGroup} from 'react-bootstrap';
+import {Col, Form, FormControl, Row} from 'react-bootstrap';
 // import Calendar from "../components/Calendar";
 import SuggestedPlants from '../components/SuggestedPlants.js'
-import Weather from "../components/Weather";
 
 
 export default class Dashboard extends Component {
   constructor(props) {
     super(props);
-    console.log("Dashboard props:", props);
   }
-  
+
   // function moveMain() {
   //   var mainPanel = document.querySelector("#mainPanel");
   //   mainPanel.style.right = 
   // }
 
   render() {
-    return (<div>
-              <div className="dashboard">
+    return (
+      <div>
+        <div className="dashboard">
           <div className="mainPanel" id="mainPanel">
             <div className="leftCard">
               <h6 className="leftCardTitle">Plants &amp; Produce</h6>
               <div className="searchBox">
                 <Form inline>
-                  <FormControl type="text" placeholder="search for tasty plants" className="mr-sm-2 plantSearch" />
+                  <FormControl type="text" placeholder="search for tasty plants" className="mr-sm-2 plantSearch"/>
                 </Form>
-                <img src={search} className='search' alt='search' /> 
+                <img src={search} className='search' alt='search'/>
               </div>
               <Row>
                 <Col><h4 className="plantOptions">my plants</h4></Col>
                 <Col><h4 className="plantOptions">suggested</h4></Col>
                 <Col><h4 className="plantOptions">all plants</h4></Col>
               </Row>
-              <hr className="h-line hl1" />
-              <hr className="h-line hl2" />
-                <hr className="suggestedBar" />
-                <SuggestedPlants /> 
+              <hr className="h-line hl1"/>
+              <hr className="h-line hl2"/>
+              <hr className="suggestedBar"/>
+              <SuggestedPlants/>
             </div>
             <div className="rightCards">
               <div className="gardenPlanner">
                 <h6 className="gardenPlannerTitle">Garden Planner</h6>
-                <hr className="h-line hl3" />
+                <hr className="h-line hl3"/>
                 <div className="calendar">
                   {/* <Calendar /> */}
                 </div>
@@ -64,18 +62,20 @@ export default class Dashboard extends Component {
             </div>
           </div>
           <div className="ProfilePanel">
-            <img src={profile} className="profile" alt="profile picture" />
+            <img src={profile} className="profile" alt="profile picture"/>
 
             <div className="profile-options">
               <Row>
                 <Col className="labelCol">
-                  <p3>settings</p3><br/>
-                  <p3>logout</p3><br/>
-                  <p3>info</p3>
+                  <p>settings</p>
+                  <br/>
+                  <p>logout</p>
+                  <br/>
+                  <p>info</p>
                 </Col>
                 <Col className="symbolCol">
                   <img src={settings} className="settings"/>
-                  <img src={logout} className="logout"/>
+                  <img src={logout} className="logout" onClick={this.props.handleLogout}/>
                   <img src={info} className="info"/>
                 </Col>
               </Row>
@@ -83,15 +83,15 @@ export default class Dashboard extends Component {
             </div>
           </div>
         </div>
-      <div>
-        <h2>Current user:</h2>
-        <ul>
-          {Object.entries(this.props.user).map((e, i) => <li key={i}>{e[0]}: {e[1]}</li>)}
-        </ul>
-        <Weather zip_code={this.props.user.zip_code}/>
-        <a href="#" className="btn" onClick={this.props.handleLogout}>Log out</a>
+        {/*<div>*/}
+        {/*  <h2>Current user:</h2>*/}
+        {/*  <ul>*/}
+        {/*    {Object.entries(this.props.user).map((e, i) => <li key={i}>{e[0]}: {e[1]}</li>)}*/}
+        {/*  </ul>*/}
+        {/*  <Weather zip_code={this.props.user.zip_code}/>*/}
+        {/*  <a href="#" className="btn" onClick={this.props.handleLogout}>Log out</a>*/}
+        {/*</div>*/}
       </div>
-  </div>
     );
   }
 }
