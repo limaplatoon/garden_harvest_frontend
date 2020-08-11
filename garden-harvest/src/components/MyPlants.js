e68f219b059199c038fdebe21c825a1d87b55257
merrillosh <oshaughnessy.merrill@gmail.com>
2020-08-11 17:25:01 -0500

    fixed search and all(?) details should be in place but i don't want to jinx it

--- garden-harvest/src/components/MyPlants.js
+++ garden-harvest/src/components/MyPlants.js
@@ -13,7 +13,7 @@ export default function MyPlants() {
         setMyPlants(data);
       })
   }, []);
-  let plants = myPlants.reverse().map((plant, i) => <PlantCard key={i} {...plant} />);
+  let plants = myPlants.reverse().map((plant, i) => <PlantCard key={i} plant={plant} />);
 
   if (plants.length === 0) {
     plants = <h4>You haven't added any plants to your garden yet.</h4>
